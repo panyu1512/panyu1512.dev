ea386c7a34a456da51d7de9678b4492f22a19381
panyu1512 <kikeferreragius1997@gmail.com>
2023-09-06 20:28:25 +0200

    Create buttons in intro page

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,10 +2,12 @@
 import {Avatar} from "@nextui-org/react"
 import React from "react"
 import Typed from "typed.js";
-import { useEffect, useRef } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import {Button, Link} from "@nextui-org/react";
+import { GithubIcon, ArrowIcon} from "@/components/icons";
+import Confetti from "@/components/confetti";
 export const Intro = () => {
-
+    const [isVisible, setIsVisible] = useState(false);
     const el = useRef(null);
     useEffect(() => {
         const typed = new Typed(el.current, {
@@ -21,20 +23,30 @@ export const Intro = () => {
     }, []);
 
     return (
-        <div>
-            <section>
-                <div className="flex flex-col text-center items-center justify-center">
-                    <div>
-                        <Avatar className="mb-4" isBordered color="secondary" size="lg" src="https://media.licdn.com/dms/image/D4D03AQH2JFOaJ3rGfg/profile-displayphoto-shrink_200_200/0/1665775544515?e=1698278400&v=beta&t=J_-YROKiEXPlwlPGvHpHBNiCtNlnL7vZ2s90sNKrQoE" />
-                    </div>
-                    <div>
-                        <h1 className="text-4xl mb-4">Hi, my name is Enrique</h1>
-                    </div>  
-                    <div>
-                        <span className="text-xl text-[#9028A3]" ref={el}></span>
-                    </div>
+        <section>
+            <div className="flex flex-col text-center items-center justify-center">
+                <div>
+                    <Avatar className="my-4" isBordered color="secondary" size="lg" src="https://media.licdn.com/dms/image/D4D03AQH2JFOaJ3rGfg/profile-displayphoto-shrink_200_200/0/1665775544515?e=1698278400&v=beta&t=J_-YROKiEXPlwlPGvHpHBNiCtNlnL7vZ2s90sNKrQoE" />
                 </div>
-            </section>
-        </div>
+                <div>
+                    <h1 className="text-4xl my-4">Hi, my name is Enrique</h1>
+                </div>  
+                <div>
+                    <span className="my-4 text-xl" ref={el}></span>
+                </div>
+            </div>
+            <div className="justify-end space-x-6 my-4">
+                <Button color="secondary" variant="shadow">
+                    Contact me here <ArrowIcon />
+                </Button>  
+                <Button onPress={() => setIsVisible(true)} color="secondary" variant="ghost" as={Link} href="/next.svg" download >
+                    Download CV
+                </Button>
+                {isVisible && <Confetti />}
+                <Button color="secondary" variant="ghost" as={Link} href="https://github.com/panyu1512" target="_blank">
+                    Github <GithubIcon/>
+                </Button>
+            </div>
+        </section>
     )
 }
