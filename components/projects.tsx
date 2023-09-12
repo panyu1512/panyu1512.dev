860528113955b8d33c424773afadd548019a1187
panyu1512 <kikeferreragius1997@gmail.com>
2023-09-12 19:29:23 +0200

    Add grid to proyect section

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,7 +10,7 @@ export default function Projects() {
   return (
     <section id="projects" className="scroll-mt-28 mb-28">
       <SectionHeading>My projects</SectionHeading>
-      <div>
+      <div className="gap-4 grid lg:grid-cols-2 sm:col-span-12">
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
