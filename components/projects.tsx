5d036ba4ecf0b78cd28bb302bc59df75b6095030
Enrique Ferrer Agius <eferreragius.external@epo.org>
2024-02-21 01:22:09 +0100

    Update grid layout in Projects component

--- components/projects.tsx
+++ components/projects.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
 
   return (
     <section id="projects" className="scroll-mt-28 mb-28">
-      <div className="gap-12 grid grid-cols-2 sm:col-span-12">
+      <div className="sm:gap-12 grid sm:grid-cols-2 sm:col-span-12 space-6-2">
         {projectsData.map((project, index) => (
           <React.Fragment key={index}>
             <Project {...project} />
